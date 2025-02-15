01374025ba19ea05512bd52b735715c5298fa588
DayZer1 <maksim-kry@mail.ru>
2025-02-16 00:02:58 +0300

    Update theme.py

--- src/theme.py
+++ src/theme.py
@@ -7,23 +7,11 @@ def main(page: ft.Page):
     
     def change_th(e):
         page.theme_mode = "light" if page.theme_mode == "dark" else "dark"
-        page.update()     
-
-    def toggle_icon(e):
-        if page.theme_mode == 'light':
-            ft.IconButton(
-            icon = ft.icons.LIGHT_MODE,
-            on_click=change_th)
-            page.update()
-        else:
-            ft.IconButton(
-            icon = ft.icons.DARK_MODE,
-            on_click=change_th)
-            page.update()
+        page.update()    
 
     page.add(
         ft.Row([
-            ft.IconButton(ft.icons.DARK_MODE, 
+            ft.IconButton(ft.icons.LIGHT_MODE, 
                 on_click=change_th)]))
 
     
